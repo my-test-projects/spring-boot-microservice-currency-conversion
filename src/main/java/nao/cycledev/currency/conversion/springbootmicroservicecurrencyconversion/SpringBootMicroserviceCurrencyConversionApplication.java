868cbd57ce9fbeb0979c502526b603e18b4d204e
noprysk <oprysk.nazar@gmail.com>
2018-09-10 20:04:20 -0600

    Init commit.

---
+++ src/main/java/nao/cycledev/currency/conversion/springbootmicroservicecurrencyconversion/SpringBootMicroserviceCurrencyConversionApplication.java
@@ -0,0 +1,12 @@
+package nao.cycledev.currency.conversion.springbootmicroservicecurrencyconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceCurrencyConversionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMicroserviceCurrencyConversionApplication.class, args);
+    }
+}
