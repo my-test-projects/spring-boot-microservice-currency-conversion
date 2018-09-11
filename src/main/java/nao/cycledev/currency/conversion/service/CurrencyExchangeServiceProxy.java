aa3807f10d76f6d679fb13cb861102d99dd9fac5
noprysk <oprysk.nazar@gmail.com>
2018-09-10 20:48:22 -0600

    Added ribbon for load balansing.

--- src/main/java/nao/cycledev/currency/conversion/service/CurrencyExchangeServiceProxy.java
+++ src/main/java/nao/cycledev/currency/conversion/service/CurrencyExchangeServiceProxy.java
@@ -1,13 +1,15 @@
 package nao.cycledev.currency.conversion.service;
 
 import nao.cycledev.currency.conversion.modal.CurrencyConversionValue;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
-@FeignClient(name="forex-service", url="localhost:8000")
+@FeignClient(name="forex-service")
+@RibbonClient(name="forex-service")
 public interface CurrencyExchangeServiceProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
